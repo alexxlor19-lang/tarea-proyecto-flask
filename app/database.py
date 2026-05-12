62f2d5135f08c17b478e5ba6c8c95d414da4e84e
alexxlor19-lang <alexxlor19@gmail.com>
2026-05-12 17:59:15 -0500

    usuario_correc2

--- app/database.py
+++ app/database.py
@@ -12,8 +12,8 @@ def get_db_connection():
 # Simulación de base de datos en memoria
 users_db = [
     {"id": 1, "username": "jorge", "email": "jorge@example.com", "password": "123"},
-    {"id": 2, "username": "pepe", "email": "pepe@example.com", "password": "456"}
+    {"id": 2, "username": "pepe", "email": "pepe@example.com", "password": "456"},
     {"id": 3, "username": "Alex", "email": "alex@example.com", "password": "789"},
-    {"id": 4, "username": "Juan", "email": "juan@example.com", "password": "987"}
+    {"id": 4, "username": "Juan", "email": "juan@example.com", "password": "987"},
     {"id": 5, "username": "Pedro", "email": "pedro@example.com", "password": "654"}
 ]
